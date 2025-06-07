c0b74d3b3f74e92188b757be930fb8e0c8b4c54f
ludoc0de <champi.ludo@tutanota.com>
2025-06-06 22:24:07 -0300

    up

--- ex.py
+++ ex.py
@@ -113,9 +113,8 @@
 #     words = line.split()
 #     if len(words) == 0:
 #         continue
-#     if words[0] != "From":
-#         continue
-#     count = count + 1
+#     if words[0] == "From"
+#     count += 1
 #     print(words[1])
 
 # print("There were", count, "lines in the file with From as the first word")
