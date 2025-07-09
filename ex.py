b01b78266204cf986e2f6db287b79cbb37a25c8f
ludoc0de <champi.ludo@tutanota.com>
2025-07-09 15:54:02 -0300

    up

--- ex.py
+++ ex.py
@@ -290,11 +290,33 @@ html = urllib.request.urlopen(url, context=ctx).read()
 soup = BeautifulSoup(html, "html.parser")
 
 # Retrieve all of the anchor tags
-count = 0
-tags = soup("a")
-for tag in tags:
-    count += 1
-    # print(tag.get("href", None), count)
-    if count == 18:
-        link = tag.get("href", None)
-        print(link)
+# count = 0
+# tags = soup("a")
+# for tag in tags:
+#     count += 1
+#     # print(tag.get("href", None), count)
+#     if count == 18:
+#         link = tag.get("href", None)
+#         print(link)
+
+import urllib.request
+import xml.etree.ElementTree as ET
+
+url = input("Enter location: ")
+if len(url) < 1:
+    url = "http://py4e-data.dr-chuck.net/comments_2225871.xml"
+
+print("Retrieving", url)
+uh = urllib.request.urlopen(url)
+data = uh.read()
+print("Retrieved", len(data), "characters")
+tree = ET.fromstring(data)
+
+counts = tree.findall(".//count")
+nums = list()
+for result in counts:
+    print(result.text)
+    nums.append(int(result.text))
+
+print("Count:", len(nums))
+print("Sum:", sum(nums))
