99a5ab3ea2d33e80f063c30bd74d74a7b39f8166
ludoc0de <champi.ludo@tutanota.com>
2025-07-31 22:04:01 -0300

    up

--- emaildb.py
+++ emaildb.py
@@ -41,25 +41,27 @@
 
 import sqlite3
 
-conn = sqlite3.connect("orgdb.sqlite")
+conn = sqlite3.connect("emaildb.sqlite")
 cur = conn.cursor()
 
-cur.execute("DROP TABLE IF EXISTS Counts")
+# cur.execute("DROP TABLE IF EXISTS Counts")
 
-cur.execute(
-    """
-CREATE TABLE Counts (org TEXT, count INTEGER)"""
-)
-
+# cur.execute(
+#     """
+# CREATE TABLE Counts (org TEXT, count INTEGER)"""
+# )
 fname = input("Enter file name: ")
 if len(fname) < 1:
     fname = "mbox.txt"
 fh = open(fname)
+lst = list()
 for line in fh:
     if not line.startswith("From: "):
         continue
     pieces = line.split()
-    org = pieces[1]
+    email = pieces[1]
+    org = email.split("@")[1]
+    print(org)
     cur.execute("SELECT count FROM Counts WHERE org = ? ", (org,))
     row = cur.fetchone()
     if row is None:
@@ -70,12 +72,13 @@ for line in fh:
         )
     else:
         cur.execute("UPDATE Counts SET count = count + 1 WHERE org = ?", (org,))
-    conn.commit()
+    # conn.commit()
+conn.commit()
 
 # https://www.sqlite.org/lang_select.html
 sqlstr = "SELECT org, count FROM Counts ORDER BY count DESC LIMIT 10"
 
+
 for row in cur.execute(sqlstr):
     print(str(row[0]), row[1])
-
 cur.close()
